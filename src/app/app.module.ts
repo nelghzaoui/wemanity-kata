a7b3a95a2c94d44eb0e9df7a90f7ce3fef2633f6
Nasreddine <elghzaouinasreddine@hotmail.com>
2019-09-15 19:59:08 +0200

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,46 @@
 import { AppRoutingModule } from './routes/app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/* PAGES */
 import { AddContactComponent } from 'pages/contact/add/add-contact.component';
 import { AppComponent } from './app.component';
 import { EditContactComponent } from 'pages/contact/edit/edit-contact.component';
 import { HomeComponent } from 'pages/home/home.component';
 import { NotFoundComponent } from 'pages/not-found/not-found.component';
 
+/* MATERIAL COMPONENT */
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
+
 @NgModule({
-  declarations: [AppModule.COMPONENT],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppModule.PAGES],
+  imports: [BrowserAnimationsModule, BrowserModule, AppModule.MATERIALS, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  static COMPONENT = [
+  private static PAGES = [
     AddContactComponent,
     AppComponent,
     EditContactComponent,
     HomeComponent,
     NotFoundComponent
+  ];
+
+  private static MATERIALS = [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatTableModule
   ];
 }
