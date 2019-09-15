67ce8b36f97e52c931d43bb508a5d3034572cae4
Nasreddine <elghzaouinasreddine@hotmail.com>
2019-09-15 03:18:32 +0200

    set the routing of the application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,47 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AddContactComponent } from './contact/add-contact/add-contact.component';
+import { EditContactComponent } from './contact/edit-contact/edit-contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'add'
+      },
+      {
+        path: 'add',
+        component: AddContactComponent
+      },
+      {
+        path: 'edit/:id',
+        component: EditContactComponent
+      }
+    ]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
