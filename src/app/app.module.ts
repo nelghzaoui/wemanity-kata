df14e4d241340a6fdc090acd60d7a1e3e50e2395
Nasreddine <elghzaouinasreddine@hotmail.com>
2019-09-15 22:17:58 +0200

    add contact page : form validation and gui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { AppRoutingModule } from './routes/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 /* PAGES */
 import { AddContactComponent } from 'pages/contact/add/add-contact.component';
@@ -12,17 +13,21 @@ import { NotFoundComponent } from 'pages/not-found/not-found.component';
 
 /* MATERIAL COMPONENT */
 import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [AppModule.PAGES],
-  imports: [BrowserAnimationsModule, BrowserModule, AppModule.MATERIALS, AppRoutingModule],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppModule.MATERIALS,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/pages/contact/add/add-contact.component.ts
+++ src/app/pages/contact/add/add-contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Contact } from 'models/contact/contact.interface';
 
 @Component({
   selector: 'app-add-contact',
@@ -6,7 +8,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-contact.component.scss']
 })
 export class AddContactComponent implements OnInit {
-  constructor() {}
+  form: FormGroup;
 
-  ngOnInit() {}
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      lastname: ['', Validators.required],
+      firstname: ['', Validators.required],
+      phonenumber: [
+        '',
+        [Validators.required, Validators.pattern('[0-9 +]*'), Validators.minLength(10)]
+      ]
+    });
+  }
+
+  onSave() {
+    if (this.form.valid) {
+      //TODO:CALL TO ADD USER IN DATABASE
+    }
+  }
+
+  getErrorMessage(fieldName: string) {
+    switch (fieldName) {
+      case 'lastname':
+        return this.lastname.hasError('required') ? 'You must enter a value' : '';
+      case 'firstname':
+        return this.firstname.hasError('required') ? 'You must enter a value' : '';
+      case 'phonenumber':
+        return this.phonenumber.hasError('required')
+          ? 'You must enter a value'
+          : this.phonenumber.hasError('pattern')
+          ? 'Not a valid entry'
+          : '';
+      default:
+        console.log('Something went wrong !');
+        break;
+    }
+  }
+
+  get firstname() {
+    return this.form.get('firstname');
+  }
+  get lastname() {
+    return this.form.get('lastname');
+  }
+  get phonenumber() {
+    return this.form.get('phonenumber');
+  }
 }
