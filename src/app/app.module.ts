f0e77e2f0537c0b2c3a5b5b4951d7a94037b007a
Nasreddine <elghzaouinasreddine@hotmail.com>
2019-09-15 03:17:33 +0200

    generate components and import them in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AddContactComponent } from './contact/add-contact/add-contact.component';
 import { AppComponent } from './app.component';
+import { EditContactComponent } from './contact/edit-contact/edit-contact.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppModule.COMPONENT],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  static COMPONENT = [
+    AddContactComponent,
+    AppComponent,
+    EditContactComponent,
+    HomeComponent,
+    NotFoundComponent
+  ];
+}
---
+++ src/app/contact/edit-contact/edit-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-contact',
+  templateUrl: './edit-contact.component.html',
+  styleUrls: ['./edit-contact.component.sass']
+})
+export class EditContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
